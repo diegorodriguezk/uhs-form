249d810f5b97e5d8b970fd40b8a7b5b51772a9ec
diegorodriguezk <diegorodriguezkk@gmail.com>
2021-10-17 21:47:08 +0000

    fix form

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+var survey_options = document.getElementById("survey_options");
+var add_more_fields = document.getElementById("add_more_fields");
+var remove_fields = document.getElementById("remove_fields");
+
+add_more_fields.onclick = function() {
+  var newField = document.createElement("input");
+  newField.setAttribute("type", "text");
+  newField.setAttribute("name", "survey_options[]");
+  newField.setAttribute("class", "ft");
+  newField.setAttribute("siz", "50");
+  newField.setAttribute("placeholder", "Part Number");
+  survey_options.appendChild(newField);
+  var newField = document.createElement("input");
+  newField.setAttribute("type", "text");
+  newField.setAttribute("name", "survey_options[]");
+  newField.setAttribute("class", "ft");
+  newField.setAttribute("siz", "50");
+  newField.setAttribute("placeholder", "Brand / Quantity ");
+  survey_options.appendChild(newField);
 };
+
+// remove_fields.onclick = function() {
+//   var input_tags = survey_options.getElementsByTagName("input");
+//   if (input_tags.length > 2) {
+//     survey_options.removeChild(input_tags() - 1);
+//   }
+// };
